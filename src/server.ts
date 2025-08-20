cec52171f07ce3598a3aa259af8e9890cc654c23
rahulk31 <rahulsingh3118.rs@gmail.com>
2025-08-20 07:31:57 +0530

    feat: add logger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import app from './app';
 import { Config } from './config';
+import logger from './config/logger';
 
 const startServer = () => {
   const PORT = Config.PORT;
   try {
-    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+    app.listen(PORT, () => logger.info(`Server is running on port ${PORT}`));
   } catch (error) {
-    console.error('Error starting server:', error);
+    logger.error('Error starting server:', error);
   }
 };
 
